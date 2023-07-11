b304be12ed5e6dfe3ac69ca98dc6ff233adac47f
Colin Barrett-Fox <colinbarrettfox@gmail.com>
2023-07-10 17:49:30 -0700

    Update ssh-keys.sh
    Trying to add an attribution

--- ssh-keys.sh
+++ ssh-keys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# The following was referenced from here: https://unix.stackexchange.com/questions/69314/automated-ssh-keygen-without-passphrase-how
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 cat ~/.ssh/id_rsa >> ~/.ssh/authorized_keys
