658e58257597b727a157b9ee8a681f2fb66d1e6d
Colin Barrett-Fox <colinbarrettfox@gmail.com>
2023-07-16 12:20:13 -0700

    Update startx-once.sh
    cleanup docs

--- startx-once.sh
+++ startx-once.sh
@@ -4,7 +4,6 @@ if [[ -f "$FILE" ]]; then
     echo "$FILE exist"
     echo "PID: " `cat $FILE`
 else
-    # Fell down a rabbit hole and definitely wanted to block xvfb from network, probably ridiculous
     # https://superuser.com/questions/855019/make-xvfb-listen-only-on-local-ip
     Xvfb -nolisten tcp $DISPLAY &
     echo $? > $FILE
